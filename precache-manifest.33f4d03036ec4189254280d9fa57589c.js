38e24fd279335dba93a1624a0f44e99702c0df73
Matthew Crompton <MLCrompton@matt-mb-air.localdomain>
2020-02-24 17:17:53 +0100

    Updates

---
+++ precache-manifest.33f4d03036ec4189254280d9fa57589c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6bc9cac2df3ba100cb8efcd15828c3",
+    "url": "/muzika/index.html"
+  },
+  {
+    "revision": "aa5b3144b4a43b65f848",
+    "url": "/muzika/static/css/main.e6e2bdfe.chunk.css"
+  },
+  {
+    "revision": "61d210dd9e893fc19562",
+    "url": "/muzika/static/js/2.d02494ee.chunk.js"
+  },
+  {
+    "revision": "0a81952dc10a472cd35dc41bef0aa2b7",
+    "url": "/muzika/static/js/2.d02494ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa5b3144b4a43b65f848",
+    "url": "/muzika/static/js/main.07a31b01.chunk.js"
+  },
+  {
+    "revision": "f9beccf06de17f4efa9c",
+    "url": "/muzika/static/js/runtime-main.e91546fe.js"
+  }
+]);
