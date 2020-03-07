498880ef238dfdfca343a5543da8419ade8c14e5
Matthew Crompton <MLCrompton@matt-mb-air.localdomain>
2020-03-07 13:37:50 +0100

    Clean up logic in Blocks

--- src/GameContainer/Blocks.js
+++ src/GameContainer/Blocks.js
@@ -1,38 +1,32 @@
 import React from "react";
 import "../App.css";
 
-const BlockMusical = ({ measure, number, handleBlockClick, children }) => {
+// prettier-ignore
+const DISPLAY_NAMES = ["I", "IV", "V", "I", null, "???"];
+
+const BlockMusical = ({ measure, number, handleBlockClick, name }) => {
   let classNameM = "";
-  number === 5 && children !== "???" && (classNameM = "mysteryBlock");
+  number === 5 && name !== "???" && (classNameM = "mysteryBlock");
   return (
     <div
       onClick={() => handleBlockClick(number)}
       className={measure === number ? "musicBlockActive" : "musicBlock"}
     >
-      <span className={classNameM}>{children}</span>
+      <span className={classNameM}>{name}</span>
     </div>
   );
 };
 
-const BlockEmpty = () => <div></div>;
-
 const Blocks = ({ measure, handleBlockClick, questionBlock }) => {
-  // prettier-ignore
-  const displayNames = ["I", "IV", "V", "I", 0, "???"]
-
-  return displayNames.map((blockname, i) => {
-    let name = blockname;
-    if (blockname === "???") {
-      name = questionBlock;
-    }
-    if (!blockname) {
-      return <BlockEmpty key={i} />;
-    }
-    return (
-      <BlockMusical key={i} number={i} {...{ handleBlockClick, measure }}>
-        {name}
-      </BlockMusical>
-    );
+  return DISPLAY_NAMES.map((name, i) => {
+    name = name === "???" ? questionBlock : name;
+    return name ? (
+      <BlockMusical
+        key={i}
+        number={i}
+        {...{ handleBlockClick, measure, name }}
+      />
+    ) : null;
   });
 };
 
