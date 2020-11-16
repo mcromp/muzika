2e2e53256cf6f7bf72397efc6b7ccfeb9e850c28
Matthew Crompton <crompton.ml@gmail.com>
2020-11-16 15:37:58 +0100

    Update about text

--- src/About.js
+++ src/About.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 const About = () => (
-  <div className="textContainer">
-    <h3>About</h3>
-    <p>This program was created by Matthew Crompton</p>
-  </div>
+ <div className="textContainer">
+  <h3>About</h3>
+  <p>This program was created by Matthew Crompton</p>
+  <p>
+   <a href="https://github.com/mcromp/muzika">Link to github repo here!</a>
+  </p>
+  <br />
+
+  <p>
+   Find me at <a href="https://www.linkedin.com/in/mcromp/"> Linkedin</a>
+   aswell
+  </p>
+ </div>
 );
 
 export default About;
