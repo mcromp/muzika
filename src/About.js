a2c6e07027e0e25a666ad10d67615358689c4b63
Matthew Crompton <crompton.ml@gmail.com>
2021-04-09 14:08:47 +0200

    Update styling for bester responsiveness

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => (
  <div className="textContainer">
   <h3>About</h3>
-  <p>This program was created by Matthew Crompton</p>
+  <p>Created by Matthew Crompton</p>
   <p>
    <a href="https://github.com/mcromp/muzika">Link to github repo here!</a>
   </p>
